344aad0efa7bf76a025e943cee946ea390fd3d19
180594 <29461627amira@gmail.com>
2023-04-24 20:26:36 +0200

    'first'

---
+++ serv.js
@@ -0,0 +1,5 @@
+var http = require("http");
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'Hello Node!!!!'});
+ }).listen(3000);
+ console.log('Server running at http://127.0.0.1:3000/');
